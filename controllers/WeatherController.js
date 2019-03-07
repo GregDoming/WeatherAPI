2b34e39db4e45e6493590be164ebd40ea8d2af1a
GregDoming <greg.domingue1@gmail.com>
2019-03-06 21:58:23 -0800

    Realized yahoo's woeid was deprecated so I am now looking up the location by city.

--- controllers/WeatherController.js
+++ controllers/WeatherController.js
@@ -5,45 +5,47 @@ const inputHelper = require('../helpers/inputHelper.js');
 
 require('dotenv').config();
 
-
-
-const getWeatherByLatLng = (latLngObj) => {
-
+const getWeatherByCity = (city) => {
   const metaWeatherOptions = {
-    uri: `https://www.metaweather.com/api/location/search/?query=${latLngObj.lat},${latLngObj.lng}`,
+    uri: `https://www.metaweather.com/api/location/search/?lattlong=${city}`,
     headers: {
       'User-Agent': 'Request-Promise',
     },
-    json: true, // Automatically parses the JSON string in the response
+    json: true,
   };
 
-  rp(metaWeatherOptions)
+  return rp(metaWeatherOptions)
     .then((data) => {
-    })
-    .catch((err) => {
-      console.error(err);
-    });
-}
-
-const getLatLongfromAddress = (str) => {
-  const location = inputHelper.parseLocationInput(str);
-
-  const mapQuestOptions = {
-    uri: `https://www.mapquestapi.com/geocoding/v1/address?key=${process.env.MAPQUEST_KEY}&inFormat=kvp&outFormat=json&location=${location}thumbMaps=false`,
-    headers: {
-      'User-Agent': 'Request-Promise',
-    },
-    json: true, // Automatically parses the JSON string in the response
-  };
-
-  return rp(mapQuestOptions)
-    .then((data) => {
-      const { latLng } = data.results[0].locations[0];
-      return latLng;
+      console.log(data)
     })
     .catch((err) => {
       console.error(err);
     });
 };
 
-getLatLongfromAddress('chicago').then(data => console.log(data));
+const getCityfromAddress = (str) => {
+  const location = inputHelper.parseLocationInput(str);
+
+  const mapQuestOptions = {
+    uri: `https://www.mapquestapi.com/geocoding/v1/address?key=${process.env.MAPQUEST_KEY}&inFormat=kvp&outFormat=json&location=${location}&thumbMaps=false`,
+    headers: {
+      'User-Agent': 'Request-Promise',
+    },
+    json: true,
+  };
+
+  return rp(mapQuestOptions)
+    .then((data) => { 
+      const city = data.results[0].locations[0].adminArea5;
+      return city;
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+};
+
+module.exports = {
+  getCityfromAddress,
+  getWeatherByCity,
+}
+
