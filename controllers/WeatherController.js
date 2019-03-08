e8cc7cffa8df9fa9b7b44d3747cd14f93e7ce64b
GregDoming <greg.domingue1@gmail.com>
2019-03-08 13:20:59 -0800

    added some comments and function definitions

--- controllers/WeatherController.js
+++ controllers/WeatherController.js
@@ -4,6 +4,51 @@ const rp = require('request-promise');
 const inputHelper = require('../helpers/inputHelper.js');
 
 require('dotenv').config();
+/**
+ * Returns 401 if authorization header does not match password
+ * @param {string} req.headers.authorization Checks the authorization header
+ * to see if the super secret password '12345' was included
+ */
+
+const checkAuthorization = (req, res, next) => {
+  if (req.headers.authorization !== '12345') res.status(401).send({ message: 'Unauthorized User' });
+  next();
+};
+
+/**
+ * Takes an address and returns the city nearest to the coordinates according to Mapquest
+ * @param {string} req.query A query string sent with the user's input address
+ */
+
+const getCityfromAddress = async (req, res, next) => {
+  const queryString = Object.keys(req.query);
+  const location = inputHelper.parseLocationInput(queryString[0]);
+  const mapQuestOptions = {
+    uri: `https://www.mapquestapi.com/geocoding/v1/address?key=${process.env.MAPQUEST_KEY}&inFormat=kvp&outFormat=json&location=${location}&thumbMaps=false`,
+    headers: {
+      'User-Agent': 'Request-Promise',
+    },
+    json: true,
+  };
+  console.log(req.headers.authorization);
+  
+  await rp(mapQuestOptions)
+  .then((address) => {
+    res.locals.city = address.results[0].locations[0].adminArea5;
+    next();
+  })
+  .catch((err) => {
+    console.log('error 3');
+  });
+  if (!res.locals.city) res.status(202).send({ message: 'Invalid Input' });
+};
+
+
+/**
+ * Takes in the name of a city and returns a woeid for the city if it is in the Metaweather database
+ * @param {string} res.locals.city A string representing the
+ * city closest to the search results from getCityFroMAddress.
+ */
 
 const getWeatherByCity = async (req, res) => {
   const woeidUri = `https://www.metaweather.com/api/location/search/?query=${res.locals.city}`;
@@ -31,39 +76,10 @@ const getWeatherByCity = async (req, res) => {
     .catch((err) => {
       console.log('error 2');
     });
-    if (!weather) res.status(202).send({ message: 'Sorry I can only give the Weather to large cities.' });
+  if (!weather) res.status(202).send({ message: 'Sorry I can only give the Weather to large cities.' });
 
-    await res.send(weather);
-
+  await res.send(weather);
 };
-
-const getCityfromAddress = async (req, res, next) => {
-  const queryString = Object.keys(req.query);
-  const location = inputHelper.parseLocationInput(queryString[0]);
-  const mapQuestOptions = {
-    uri: `https://www.mapquestapi.com/geocoding/v1/address?key=${process.env.MAPQUEST_KEY}&inFormat=kvp&outFormat=json&location=${location}&thumbMaps=false`,
-    headers: {
-      'User-Agent': 'Request-Promise',
-    },
-    json: true,
-  };
-  console.log(req.headers.authorization)
-
-  await rp(mapQuestOptions)
-    .then((address) => {
-      res.locals.city = address.results[0].locations[0].adminArea5;
-      next();
-    })
-    .catch((err) => {
-      console.log('error 3');
-    });
-  if (!res.locals.city) res.status(202).send({ message: 'Invalid Input' });
-};
-
-const checkAuthorization = (req, res, next) => {
-  if (req.headers.authorization !== '12345') res.status(401).send({ message: 'Unauthorized User' })
-  next();
-}
 
 module.exports = {
   checkAuthorization,
--- server.js
+++ server.js
@@ -7,14 +7,11 @@ const port = 4400;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(weatherController.allowCrossDomain);
 
 app.get('/', (req, res) => {
   res.send('Get request to home route reached');
 });
 
 app.get('/api/forecast', weatherController.checkAuthorization, weatherController.getCityfromAddress, weatherController.getWeatherByCity);
-
-// app.post('/api/forecast', weatherController.getCityfromAddress, weatherController.getWeatherByCity);
 
 app.listen(port, () => console.log('Listening on port 4400'));
